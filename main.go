825fc9b56ee498ba1776afe7156c4c9907421e38
Faithfulness Alamu <vaguemail369@gmail.com>
2020-12-04 19:49:14 +0100

    Move main to root folder

--- main.go
+++ main.go
@@ -1,11 +1,66 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"os"
+	"time"
 )
 
 func main() {
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func run() error {
+	clipboard := &clipboardProvider{}
+
+	addr := os.Getenv("PBGOPY_SERVER")
+	if addr == "" {
+		return errors.New("put the pbgopy server's address into PBGOPY_SERVER environment variable")
+	}
+	server := &serverProvider{addr: addr}
+
+	for {
+		time.Sleep(1 * time.Second)
+
+		if clipboard.hasNew() {
+			if clipboard.lastClipboardTs > server.lastServerTs {
+				fmt.Println("Updating server")
+				data, err := clipboard.get()
+				if err != nil {
+					showError(err)
+					continue
+				}
+				err = server.put(data)
+				if err != nil {
+					showError(err)
+					continue
+				}
+			}
+
+		} else if server.hasNew() {
+			if server.lastServerTs > clipboard.lastClipboardTs {
+				fmt.Println("Updating clipboard")
+				data, err := server.get()
+				if err != nil {
+					showError(err)
+					continue
+				}
+				err = clipboard.put(data)
+				if err != nil {
+					showError(err)
+					continue
+				}
+
+			}
+
+		}
+	}
+}
+
+func showError(err error) {
+	fmt.Fprintf(os.Stderr, err.Error())
 }
