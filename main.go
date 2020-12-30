ea63b89febb5be587b84c0c71d3f7d96252e3392
Faithfulness Alamu <vaguemail369@gmail.com>
2020-12-30 23:13:31 +0100

    Add debug logging

--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
 )
 
 func main() {
-	if err := run(); err != nil {
+	l := log.New(ioutil.Discard, "PBGOPY-CLIP: ", log.LstdFlags)
+	if os.Getenv("PBGOPY_CLIP_DEBUG") != "" {
+		l.SetOutput(os.Stdout)
+	}
+
+	if err := run(l); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run() error {
+func run(logging *log.Logger) error {
 	var clipboard provider = newClipboardProvider()
 
 	addr := os.Getenv("PBGOPY_SERVER")
@@ -23,10 +29,15 @@ func run() error {
 	}
 	var server provider = newServerProvider(addr)
 
+	logging.Printf("Server is at %s", addr)
+
 	for {
+		logging.Printf("Sleeping for 1s")
 		time.Sleep(1 * time.Second)
+		logging.Printf("Awake")
 
 		if clipboard.hasNew() && clipboard.getLastTimestamp() > server.getLastTimestamp() {
+			logging.Println("Clipboard has recent data")
 			data, err := clipboard.get()
 			if err != nil {
 				showError(err)
@@ -37,8 +48,10 @@ func run() error {
 				showError(err)
 				continue
 			}
+			logging.Println("Successfully put clipboard data in server")
 
 		} else if server.hasNew() && server.getLastTimestamp() >= clipboard.getLastTimestamp() {
+			logging.Println("Server has recent data")
 			data, err := server.get()
 			if err != nil {
 				showError(err)
@@ -49,7 +62,7 @@ func run() error {
 				showError(err)
 				continue
 			}
-
+			logging.Println("Successfully put server data in clipboard")
 		}
 
 	}
